d3f4edaa7d829b2f940b9ea8b55280796cfefa4d
lklee <lklee@SCT13561.staffdmn.ouhk.edu.hk>
2019-04-12 15:35:31 +0800

    Lab 12 project with updated create_users.sql

--- src/main/resources/create_users.sql
+++ src/main/resources/create_users.sql
@@ -12,15 +12,15 @@ CREATE TABLE user_roles (
     FOREIGN KEY (username) REFERENCES users(username)
 );
 
-INSERT INTO users VALUES ('keith', 'keithpw');
+INSERT INTO users VALUES ('keith', '$2a$10$dHpXhH42.h3E7sq4ISe2.epIvkQwlcL4pCnSPzFICf3sgFfsdRCva');
 INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_USER');
 INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_ADMIN');
 
-INSERT INTO users VALUES ('tsli', 'tslipw');
+INSERT INTO users VALUES ('tsli', '$2a$10$q6mSlksyfTK/h8l2uDS1dewsHssFIjquVflzB4ocZyXXbB1hodeDy');
 INSERT INTO user_roles(username, role) VALUES ('tsli', 'ROLE_ADMIN');
 
-INSERT INTO users VALUES ('kevin', 'kevinpw');
+INSERT INTO users VALUES ('kevin', '$2a$10$g0T2rqJT7mlxN8kHkTRJVuY2m9Bm.qWovZrDhuF0XVagfVan8AOMi');
 INSERT INTO user_roles(username, role) VALUES ('kevin', 'ROLE_USER');
 
-INSERT INTO users VALUES ('oliver', 'oliverpw');
+INSERT INTO users VALUES ('oliver', '$2a$10$bwVC2eZHRJi0p4GL8iImtuv8bft4EpD6TzvarxG.9KWzDUSshrKry');
 INSERT INTO user_roles(username, role) VALUES ('oliver', 'ROLE_USER');
